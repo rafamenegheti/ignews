cfdada75bbbbdb54a61f777b11b3033c7413ead9
Rafael Menegheti <rafael@pop-os.localdomain>
2021-12-26 18:19:47 -0300

    changing api route

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://ignews-4646p61a0-rafamenegheti.vercel.app:3000/api'
+    baseURL: 'https://ignews-4646p61a0-rafamenegheti.vercel.app/api'
 })
